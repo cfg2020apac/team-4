256787873de759e1074c91303431e1f4e86891c7
Zhi Qi Sim <zhiqisim@gmail.com>
2020-10-09 23:43:34 +0800

    api add event endpoint and annoucnemnet

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, g, send_file, session
+from flask import Flask, request, jsonify, g, send_file, session, send_from_directory, safe_join, abort
 import mysql.connector
 import json
 import jwt
@@ -7,7 +7,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+# The absolute path of the directory containing images for users to download
 app.config.from_json('config.json')
 
 @app.before_request
@@ -80,11 +81,11 @@ def signup():
 
     hashed_password = generate_password_hash(pw, method='sha256')
 
-    sql = f"INSERT INTO users (username, user_password) VALUES (%s, %s)"
+    sql = f"INSERT INTO user (username, user_password) VALUES (%s, %s)"
 
     g.cursor.execute(sql, [username, hashed_password])
 
-    sql = f"SELECT id FROM users WHERE username = (%s)"
+    sql = f"SELECT id FROM user WHERE username = (%s)"
 
     g.cursor.execute(sql, [username])
     fetched = g.cursor.fetchone()
@@ -104,7 +105,7 @@ def login():
     username = data["username"]
     pw = data["password"]
 
-    sql = f"SELECT * FROM users WHERE username = (%s) LIMIT 1"
+    sql = f"SELECT * FROM user WHERE email = (%s) LIMIT 1"
 
     g.cursor.execute(sql, [username])
 
@@ -134,4 +135,158 @@ def logout(current_user):
     response = {'id' : current_user['user_id'], 'message': 'Successfully logged out'}
     status_code = 200
     return json.dumps(response), status_code, {'Content-Type': 'json; charset=utf-8'}
+
+
+@app.route('/api/announcements', methods=['GET'])
+@authentication_required
+def announcements(current_user):
+    """
+    Input: NIL
+    Output: list of annoucements
+    """
+    response = {}
+    sql = f"SELECT title , content from announcement;"
+    g.cursor.execute(sql)
+    data = g.cursor.fetchall()
+    response_msg = list()
+
+    for row in data:
+        response_msg_link = dict()
+        response_msg_link["title"] = row[0]
+        response_msg_link["content" ] = row[1]
+
+        response_msg.append(response_msg_link)
+    response = {
+        "data": response_msg,
+        "error": None
+    }
+    status_code = 200
+    return json.dumps(response), status_code, {'Content-Type': 'json; charset=utf-8'}
+
+@app.route('/api/new-event', methods=['POST'])
+@authentication_required
+def resources(current_user):
+    """
+    Input: form data for new event
+    Output: NIL
+    """
+    uploaded_file = request.files['file']
+    name = request.form.get('name')
+    location = request.form.get('location')
+    date = request.form.get('date')
+    descriptions = request.form.get('descriptions')
+    sql = f"INSERT INTO events(event_title, location, date, descriptions, url) VALUES(%s,%s,%s,%s,%s);"
+    g.cursor.execute(sql, [name, location, date, descriptions, "localhost:5000/get-event-banner/" + uploaded_file.filename])
+    if uploaded_file.filename != '':
+            uploaded_file.save("./s3/" + uploaded_file.filename)
+    response = {
+        'data': "sucess",
+        'error': None
+    }
+    status_code = 200
+    return json.dumps(response), status_code, {'Content-Type': 'json; charset=utf-8'}
+
+
+@app.route('/api/get-event-banner/<path>', methods=['GET'])
+@authentication_required
+def get_event_banner(current_user, path):
+    """
+    Input: NIL
+    Output: url of pic from s3
+    """
+    return send_from_directory('s3', path)
+
+@app.route('/api/events', methods=['GET'])
+@authentication_required
+def events(current_user):
+    """
+    Input: NIL
+    Output: list of events
+    """
+    response = {}
+    sql = f"SELECT event_id , location, date, descriptions, url from events;"
+    g.cursor.execute(sql)
+    data = g.cursor.fetchall()
+    response_msg = list()
+
+    for row in data:
+        response_msg_link = dict()
+        response_msg_link["name"] = row[0]
+        response_msg_link["location"] = row[1]
+        response_msg_link["date"] = row[2]
+        response_msg_link["descriptipns"] = row[3]
+        response_msg_link["bannerImageUrl"] = row[4]
+        response_msg.append(response_msg_link)
+    response = {
+        "data": response_msg,
+        "error": None
+    }
+    status_code = 200
+    return json.dumps(response), status_code, {'Content-Type': 'json; charset=utf-8'}
+
+
+@app.route('/api/pet', methods=['GET'])
+@authentication_required
+def pet(current_user):
+    """
+    Input: NIL
+    Output: url of pic from s3
+    """
+    # TODO: Query DB for the s3 link
+    # response = {
+    #     'data': "url",
+    #     'error': None
+    # }
+    # status_code = 200
+    # return json.dumps(response), status_code, {'Content-Type': 'json; charset=utf-8'}
+    return send_from_directory('s3', "star.jpg")
+
+
+@app.route('/api/get-score', methods=['GET'])
+@authentication_required
+def get_score(current_user):
+    """
+    Input: NIL
+    Output: score
+    """
+    # TODO: get the score from DB
+    response = {
+        'data': "score",
+        'error': None
+    }
+    status_code = 200
+    return json.dumps(response), status_code, {'Content-Type': 'json; charset=utf-8'}
+
+
+@app.route('/api/add-score', methods=['POST'])
+@authentication_required
+def add_score(current_user):
+    """
+    Input: NIL
+    Output: score
+    """
+    # TODO: add the score to DB 
+    response = {
+        'data': "score",
+        'error': None
+    }
+    status_code = 200
+    return json.dumps(response), status_code, {'Content-Type': 'json; charset=utf-8'}
+
+
+@app.route('/api/leaderboard', methods=['GET'])
+@authentication_required
+def leaderboard(current_user):
+    """
+    Input: NIL
+    Output: score
+    """
+    # TODO: obtain all data from leaderboard
+    response = {
+        'data': "score",
+        'error': None
+    }
+    status_code = 200
+    return json.dumps(response), status_code, {'Content-Type': 'json; charset=utf-8'}
+
 
