732c3fbe99ed171c9a52d8e6ea645b8ece7c830d
Tianyi <42670338+tysng@users.noreply.github.com>
2020-10-10 03:55:28 +0800

    Hard code view event and sign up button

--- frontend/src/api/events.ts
+++ frontend/src/api/events.ts
@@ -17,18 +17,52 @@ class EventsAPI extends BaseAPI {
       messages: [],
       data: [
         {
-          name: 'test',
-          address: '1233',
-          bannerImage: 'https://place-hold.it/300x500/',
-          date: '1998-09-09',
-          descriptions: 'lorem ipsum'
+          name: 'Discovering Treasures',
+          address: 'Tohyi',
+          bannerImage:
+            'https://images.squarespace-cdn.com/content/v1/59954067bebafbb309508e10/1527241613289-MU42FY7AN7R4SJL1GLFV/ke17ZwdGBToddI8pDm48kPTrHXgsMrSIMwe6YW3w1AZ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0k5fwC0WRNFJBIXiBeNI5fKTrY37saURwPBw8fO2esROAxn-RKSrlQamlL27g22X2A/20170304_115245.jpg?format=1000w',
+          date: '2020-08-01',
+          descriptions: 'Ages 10-12, children prgm & events, @Aljunied Branch'
         },
         {
-          name: 'test',
-          address: '1233',
-          bannerImage: 'https://place-hold.it/300x500/',
-          date: '1998-09-09',
-          descriptions: 'lorem ipsum'
+          name: 'Happy Formula',
+          address: 'Tohyi',
+          bannerImage:
+            'https://images.squarespace-cdn.com/content/v1/59954067bebafbb309508e10/1527648427040-V3DEBBDZ4W87LX5870VG/ke17ZwdGBToddI8pDm48kLkXF2pIyv_F2eUT9F60jBl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0iyqMbMesKd95J-X4EagrgU9L3Sa3U8cogeb0tjXbfawd0urKshkc5MgdBeJmALQKw/shutterstock_99016145.jpg?format=750w',
+          date: '2020-08-01',
+          descriptions: 'Ages 10-12, children prgm & events, @Aljunied Branch'
+        },
+        {
+          name: '#I Am Remarkable Workshop',
+          address: 'Tohyi',
+          bannerImage:
+            'https://images.squarespace-cdn.com/content/v1/59954067bebafbb309508e10/1600144082951-H2MJY0A419MBRJ7Z9KTN/ke17ZwdGBToddI8pDm48kMjeOQW7eETwI1at-vRo8Cd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmEZprBMNaW6dUYQUrCwR2cROwLzAdC_zB1aDNzl1Yykp2YvkyzD0BchbnFDmMjIEC/WhatsApp+Image+2020-09-14+at+11.28.22+AM.jpeg?format=500w',
+          date: '2020-09-01',
+          descriptions: 'Ages 10-12, children prgm & events, @Aljunied Branch'
+        },
+        {
+          name: 'Discovering Treasures',
+          address: 'Tohyi',
+          bannerImage:
+            'https://images.squarespace-cdn.com/content/v1/59954067bebafbb309508e10/1527241613289-MU42FY7AN7R4SJL1GLFV/ke17ZwdGBToddI8pDm48kPTrHXgsMrSIMwe6YW3w1AZ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0k5fwC0WRNFJBIXiBeNI5fKTrY37saURwPBw8fO2esROAxn-RKSrlQamlL27g22X2A/20170304_115245.jpg?format=1000w',
+          date: '2020-08-01',
+          descriptions: 'Ages 10-12, children prgm & events, @Aljunied Branch'
+        },
+        {
+          name: 'Happy Formula',
+          address: 'Tohyi',
+          bannerImage:
+            'https://images.squarespace-cdn.com/content/v1/59954067bebafbb309508e10/1527648427040-V3DEBBDZ4W87LX5870VG/ke17ZwdGBToddI8pDm48kLkXF2pIyv_F2eUT9F60jBl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0iyqMbMesKd95J-X4EagrgU9L3Sa3U8cogeb0tjXbfawd0urKshkc5MgdBeJmALQKw/shutterstock_99016145.jpg?format=750w',
+          date: '2020-08-01',
+          descriptions: 'Ages 10-12, children prgm & events, @Aljunied Branch'
+        },
+        {
+          name: '#I Am Remarkable Workshop',
+          address: 'Tohyi',
+          bannerImage:
+            'https://images.squarespace-cdn.com/content/v1/59954067bebafbb309508e10/1600144082951-H2MJY0A419MBRJ7Z9KTN/ke17ZwdGBToddI8pDm48kMjeOQW7eETwI1at-vRo8Cd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmEZprBMNaW6dUYQUrCwR2cROwLzAdC_zB1aDNzl1Yykp2YvkyzD0BchbnFDmMjIEC/WhatsApp+Image+2020-09-14+at+11.28.22+AM.jpeg?format=500w',
+          date: '2020-09-01',
+          descriptions: 'Ages 10-12, children prgm & events, @Aljunied Branch'
         }
       ]
     };
--- frontend/src/modules/events/components/EventCard.tsx
+++ frontend/src/modules/events/components/EventCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -8,6 +8,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { EventDetails } from 'src/types/EventDetails';
+import CheckIcon from '@material-ui/icons/Check';
 
 const useStyles = makeStyles({
   root: {
@@ -17,6 +18,11 @@ const useStyles = makeStyles({
 
 export default function EventCard(props: EventDetails) {
   const classes = useStyles();
+  const [hasClicked, setClick] = useState(false);
+
+  const handleClick = () => {
+    setClick((s) => !s);
+  };
 
   return (
     <Card className={classes.root}>
@@ -38,9 +44,15 @@ export default function EventCard(props: EventDetails) {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size='small' color='primary'>
-          Sign Up
-        </Button>
+        {!hasClicked ? (
+          <Button size='small' color='primary' onClick={handleClick}>
+            Sign Up
+          </Button>
+        ) : (
+          <Button variant='contained' size='small' color='secondary' startIcon={<CheckIcon />} onClick={handleClick}>
+            Going
+          </Button>
+        )}
       </CardActions>
     </Card>
   );
